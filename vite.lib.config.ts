e3026ccfc30fe0966d2c71a4e21d5621a7d3d340
Andrey Shurygin <shuanvita@gmail.com>
2026-02-26 15:23:28 +0300

    Add types

--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts({ tsconfigPath: './tsconfig.app.json' })],
   publicDir: false,
   resolve: {
     alias: {
