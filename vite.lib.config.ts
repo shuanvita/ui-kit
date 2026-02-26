1c8241270e016ba29c32d295b35c38e53406402d
Andrey Shurygin <shuanvita@gmail.com>
2026-02-26 15:14:19 +0300

    Fix

--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
+  publicDir: false,
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
       fileName: 'index',
       formats: ['es', 'cjs'],
     },
