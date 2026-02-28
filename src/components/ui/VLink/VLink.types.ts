f98605266d38302bad5811b1847261786e6f06e2
Andrey Shurygin <shuanvita@gmail.com>
2026-02-28 21:00:49 +0300

    Fix VLink Types

--- src/components/ui/VLink/VLink.types.ts
+++ src/components/ui/VLink/VLink.types.ts
@@ -1,6 +1,6 @@
 import type { RouteLocationRaw } from 'vue-router'
 
-interface VLinkBase {
+export interface VLinkBase {
   icon?: string
   preIcon?: string
   postIcon?: string
@@ -9,21 +9,21 @@ interface VLinkBase {
   disabled?: boolean
 }
 
-interface ExternalLink extends VLinkBase {
+export interface ExternalLink extends VLinkBase {
   href: string
   target?: '_blank' | '_self' | '_parent' | '_top'
   rel?: string
   to?: never
 }
 
-interface InternalLink extends VLinkBase {
+export interface InternalLink extends VLinkBase {
   to: RouteLocationRaw
   href?: never
   target?: never
   rel?: never
 }
 
-interface Span extends VLinkBase {
+export interface Span extends VLinkBase {
   href?: never
   to?: never
   target?: never
